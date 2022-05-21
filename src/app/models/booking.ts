1dd446fd7083d336590cab8ab92335a62c4c1328
Ingrid <ingridleichterr@gmail.com>
2022-05-21 09:50:56 +0300

    Created booking model and component

---
+++ src/app/models/booking.ts
@@ -0,0 +1,14 @@
+
+export class Booking {
+  id?:any;
+  arrivalDate?:any;
+  leavingDate?:any;
+  numberOfPeople?:any;
+  name?:any;
+  email?:any;
+  phoneNumber?:any;
+
+// what about foregin key variables? how do i add those? do i need to add all?
+//I dont want customer to be able to set all variables, some of them should come automatically
+
+}
