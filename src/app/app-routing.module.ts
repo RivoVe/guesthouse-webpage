21822592c00d57d408f43d4cbd95461855add09f
Ingrid <ingridleichterr@gmail.com>
2022-05-26 11:33:33 +0300

    adding Menüü | Meal and finishing add-meal html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { FoodBevarageComponent } from './food-bevarage/food-bevarage.component';
 import { HomeComponent } from './home/home.component';
 import { HinnakiriComponent } from './hinnakiri/hinnakiri.component';
 import { CreateBookingComponent } from './create-booking/create-booking.component';
+import { AddMealComponent } from './meal/add-meal/add-meal.component';
 
 
 const routes: Routes = [
@@ -16,7 +17,8 @@ const routes: Routes = [
   { path: 'food-beverage', component: FoodBevarageComponent },
   { path: 'hinnakiri', component: HinnakiriComponent },
   { path: '', component: HomeComponent},
-  { path: 'booking', component: CreateBookingComponent}
+  { path: 'booking', component: CreateBookingComponent},
+  { path: 'meal', component: AddMealComponent}
 ];
 
 @NgModule({
